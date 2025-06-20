411d613d1512c2afd566aa34e050f404d91b5e4d
SerejaTsoi20050419 <mistendo12345@gmail.com>
2025-06-21 03:37:36 +0900

    first commit

---
+++ .history/src/js/index_20250620233136.js
@@ -0,0 +1,117 @@
+import '../scss/style.scss'
+
+console.log('It works!')
+
+const tabs = document.querySelectorAll('.services__tab');
+const contents = document.querySelectorAll('.services__content');
+  
+    tabs.forEach(tab => {
+      tab.addEventListener('click', function () {
+        tabs.forEach(t => t.classList.remove('services__tab--active'));
+        tab.classList.add('services__tab--active');
+  
+        contents.forEach(content => content.classList.remove('services__content--visible'));
+  
+        const selectedId = `tab-${tab.dataset.tab}`;
+        const selectedContent = document.getElementById(selectedId);
+        if (selectedContent) {
+          selectedContent.classList.add('services__content--visible');
+        }
+      });
+    });
+    
+    // Кнопка "Читать далее"
+    const readMoreBtn = document.getElementById('readMoreBtn');
+    const readMoreText = document.getElementById('readMoreText');
+    const expandIcon = document.getElementById('expandIcon');
+    const moreText = document.getElementById('moreText');
+    
+    readMoreBtn.addEventListener('click', () => {
+      const isHidden = moreText.style.display === 'none' || moreText.style.display === '';
+      if (isHidden) {
+        moreText.style.display = 'block';
+        readMoreText.textContent = 'Скрыть';
+        expandIcon.classList.add('rotate');
+      } else {
+        moreText.style.display = 'none';
+        readMoreText.textContent = 'Читать далее';
+        expandIcon.classList.remove('rotate');
+      }
+    });
+
+    const toggleButton = document.getElementById('toggle-button');
+    const toggleText = document.getElementById('toggle-text');
+    const toggleIcon = document.getElementById('toggle-icon');
+    const brandList = document.getElementById('brand-list');
+
+    toggleButton.addEventListener('click', () => {
+    const isHidden = brandList.style.display === 'none' || brandList.style.display === '';
+
+    brandList.style.display = isHidden ? 'grid' : 'none';
+    toggleText.textContent = isHidden ? 'Скрыть' : 'Показать все';
+    toggleIcon.src = isHidden ? 'img/expand.png' : 'img/thrt.png'; // замените на нужную иконку
+  });
+
+    const mainSwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1.0,
+    spaceBetween: 16,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      direction: 'vertical'
+    },
+  });
+
+const burgerBtn = document.getElementById('burger-btn');
+const sideMenu = document.getElementById('side-menu');
+const closeBtn = document.getElementById('close-btn');
+
+burgerBtn.addEventListener('click', () => {
+  sideMenu.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+  sideMenu.classList.remove('open');
+});
+
+  const repairSwiper = new Swiper('#repair-swiper', {
+    slidesPerView: 1.2,
+    spaceBetween: 15,
+    pagination: {
+      el: '.swiper-paginationser',
+      clickable: true,
+    },
+    breakpoints: {
+      430: {
+        slidesPerView: 1.0,
+      },
+    },
+  });
+
+  const anotherSwiper = new Swiper(".swiper-conta", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-paginationiio",
+      clickable: true
+    },
+  });
+
+   function toggleMore() {
+    const moreText = document.getElementById('moreText');
+    const imgBlock = document.getElementById('imgBlock');
+
+    // показать / скрыть текст
+    if (moreText.style.display === "none") {
+      moreText.style.display = "block";
+      
+      // Только на экране 430px и меньше
+      if (window.innerWidth <= 430) {
+        imgBlock.classList.add('moved');
+      }
+    } else {
+      moreText.style.display = "none";
+      imgBlock.classList.remove('moved');
+    }
+  }
---
+++ .history/src/js/index_20250620234013.js
@@ -0,0 +1,137 @@
+bimport '../scss/style.scss'
+
+console.log('It works!')
+
+const tabs = document.querySelectorAll('.services__tab');
+const contents = document.querySelectorAll('.services__content');
+  
+    tabs.forEach(tab => {
+      tab.addEventListener('click', function () {
+        tabs.forEach(t => t.classList.remove('services__tab--active'));
+        tab.classList.add('services__tab--active');
+  
+        contents.forEach(content => content.classList.remove('services__content--visible'));
+  
+        const selectedId = `tab-${tab.dataset.tab}`;
+        const selectedContent = document.getElementById(selectedId);
+        if (selectedContent) {
+          selectedContent.classList.add('services__content--visible');
+        }
+      });
+    });
+    
+  const readMoreBtn = document.getElementById("readMoreBtn");
+  const moreText = document.getElementById("moreText");
+  const readMoreText = document.getElementById("readMoreText");
+  const cpsImage = document.querySelector(".container-picture");
+
+  function isMobileView() {
+    return window.innerWidth <= 430;
+  }
+
+  function resetImageOnResize() {
+    if (!isMobileView()) {
+      // Удаляем мобильный класс при расширении окна
+      cpsImage.classList.remove("move-down");
+    }
+  }
+
+  readMoreBtn.addEventListener("click", () => {
+    const isMobile = isMobileView();
+
+    if (moreText.style.display === "none") {
+      moreText.style.display = "block";
+      readMoreText.textContent = "Скрыть";
+
+      if (isMobile) {
+        cpsImage.classList.add("move-down");
+      }
+
+    } else {
+      moreText.style.display = "none";
+      readMoreText.textContent = "Читать далее";
+      cpsImage.classList.remove("move-down");
+    }
+  });
+
+  // Отслеживаем изменение ширины окна
+  window.addEventListener("resize", resetImageOnResize);
+
+   
+
+    const toggleButton = document.getElementById('toggle-button');
+    const toggleText = document.getElementById('toggle-text');
+    const toggleIcon = document.getElementById('toggle-icon');
+    const brandList = document.getElementById('brand-list');
+
+    toggleButton.addEventListener('click', () => {
+    const isHidden = brandList.style.display === 'none' || brandList.style.display === '';
+
+    brandList.style.display = isHidden ? 'grid' : 'none';
+    toggleText.textContent = isHidden ? 'Скрыть' : 'Показать все';
+    toggleIcon.src = isHidden ? 'img/expand.png' : 'img/thrt.png'; // замените на нужную иконку
+  });
+
+    const mainSwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1.0,
+    spaceBetween: 16,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      direction: 'vertical'
+    },
+  });
+
+const burgerBtn = document.getElementById('burger-btn');
+const sideMenu = document.getElementById('side-menu');
+const closeBtn = document.getElementById('close-btn');
+
+burgerBtn.addEventListener('click', () => {
+  sideMenu.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+  sideMenu.classList.remove('open');
+});
+
+  const repairSwiper = new Swiper('#repair-swiper', {
+    slidesPerView: 1.2,
+    spaceBetween: 15,
+    pagination: {
+      el: '.swiper-paginationser',
+      clickable: true,
+    },
+    breakpoints: {
+      430: {
+        slidesPerView: 1.0,
+      },
+    },
+  });
+
+  const anotherSwiper = new Swiper(".swiper-conta", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-paginationiio",
+      clickable: true
+    },
+  });
+
+   function toggleMore() {
+    const moreText = document.getElementById('moreText');
+    const imgBlock = document.getElementById('imgBlock');
+
+    // показать / скрыть текст
+    if (moreText.style.display === "none") {
+      moreText.style.display = "block";
+      
+      // Только на экране 430px и меньше
+      if (window.innerWidth <= 430) {
+        imgBlock.classList.add('moved');
+      }
+    } else {
+      moreText.style.display = "none";
+      imgBlock.classList.remove('moved');
+    }
+  }
